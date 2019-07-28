fdad24c89117e8c0e52d05c62d146aa74900489d
Louis Malone <lmalone874@gmail.com>
2019-07-27 22:25:09 -0400

    please work commit

--- keys.js
+++ keys.js
@@ -1,6 +1,14 @@
-console.log('this is loaded');
+// API Keys for various APIs to be used in the Liri App
 
-exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
-};
+
+// various keys needed for APIs set as a constant
+const keys = {
+
+    spotify: {
+        id: "3870c791712454944b11f4e0eeac8b7c2",
+        secret: "e1e3a85f666345438c1cef4e9305da75"
+    },
+
+}
+
+module.exports = keys
