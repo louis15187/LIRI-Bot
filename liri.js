e958c8a34d390e4ce55e2ef030d0eed047af5fdb
Louis Malone <lmalone874@gmail.com>
2019-07-27 21:50:22 -0400

    all commit

--- liri.js
+++ liri.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 var keys = require('./keys');
 var Spotify = require('node-spotify-api');
 //added to format table 
-var cTable = require('console.table');
 var request = require('request');
 var moment = require('moment');
 
